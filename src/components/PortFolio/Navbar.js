85fac24602d47d1b9b9bc13063d81f89cc9b90b9
Shubham Tiwari <shubhmtiwri00@gmail.com>
2022-03-23 12:25:15 +0530

    Updated the responsiveness of contact section

--- src/components/PortFolio/Navbar.js
+++ src/components/PortFolio/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Routes, Route, Link } from 'react-router-dom'
 import RubberBand from 'react-reveal/RubberBand';
 import Flip from 'react-reveal/Flip';
